24c55dba379dc1ab9e45454cfa2b9d01aebc4b53
Afnan Enayet <afnan.enayet@gmail.com>
2016-03-09 18:30:07 -0500

    added flask webserver for camera

--- capture still/webapp.py
+++ capture still/webapp.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import picamera
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    with picamera.PiCamera() as camera:
+    camera.resolution = (2592, 1944)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(5)
+    camera.capture('temp.jpg')
+    return app.send_static_file('temp.jpg')
+        
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
